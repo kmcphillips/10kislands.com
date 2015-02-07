ed2c73de93ed0a17dd9f51f222205afbf9877f0f
Kevin McPhillips <github@kevinmcphillips.ca>
2015-02-07 18:19:53 -0500

    Link uploads

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,3 +3,5 @@ server 'staging.kevinmcphillips.ca', user: 'kevin', roles: %w{web app}, primary:
 set :rails_env, 'staging'
 set :deploy_to, "/home/kevin/staging.kevinmcphillips.ca"
 # set :branch, 'staging'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('public/system')
