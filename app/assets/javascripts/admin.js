bc893399f1b4862c991bc37aabaa754a71070f20
Kevin McPhillips <github@kevinmcphillips.ca>
2015-02-08 11:56:39 -0500

    Add image upload previewing

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,3 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function previewImage(image, previewId) {
+  var reader = new FileReader();
+  reader.readAsDataURL(image.files[0]);
+  reader.onload = function (event) {
+    document.getElementById(previewId).src = event.target.result;
+  };
+}
