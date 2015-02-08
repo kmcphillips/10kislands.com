aa64aab31a8313b7257fc7570c8a792ec5c1c551
Kevin McPhillips <github@kevinmcphillips.ca>
2015-02-07 23:27:52 -0500

    Add easy /login /logout paths

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    get "/logout" => "devise/sessions#destroy"
+  end
+
   namespace :admin do
     root "users#index"
 
