35ba6cabd2d3ef95170cf12d7c9c1cf80cf419c8
Kevin McPhillips <github@kevinmcphillips.ca>
2015-02-08 11:53:17 -0500

    Helper for card previews

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,4 +2,10 @@ module AdminHelper
   def error_messages(form)
     render "shared/error_messages", object: form.object if form.object.errors.any?
   end
+
+  def card_image_preview(card, opts={})
+    opts = {alt: card.title, style: "width: 200px; height: 200px;"}.merge(opts)
+
+    image_tag(card.background.url(:thumb), opts)
+  end
 end
--- spec/helpers/admin_helper_spec.rb
+++ spec/helpers/admin_helper_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe AdminHelper, :type => :helper do
+  let(:card){ Card.new(description: "Video\nStuff", link: "http://www.youtube.com/watch?v=asdf") }
 
+  describe "#card_image_preview" do
+    it "renders an image tag for the card with opts" do
+      expect(helper.card_image_preview(card, class: "shiny")).to eq("<img alt=\"Video Stuff\" style=\"width: 200px; height: 200px;\" class=\"shiny\" src=\"/images/thumb/missing.png\" />")
+    end
+  end
 end
