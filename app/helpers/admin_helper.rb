a494bb1647e3b3acbc293a1b6b90e41aba603e90
Kevin McPhillips <github@kevinmcphillips.ca>
2015-04-05 20:51:14 -0400

    Fix iteration. Use clearer arrows.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -8,4 +8,26 @@ module AdminHelper
 
     image_tag(card.background.url(:thumb), opts)
   end
+
+  def higher_arrow_for_index(index)
+    case index
+    when 0, 2, 3, 5
+      "up"
+    when 1
+      "left"
+    when 4
+      "right"
+    end
+  end
+
+  def lower_arrow_for_index(index)
+    case index
+    when 1, 2, 4, 5
+      "down"
+    when 3
+      "left"
+    when 0
+      "right"
+    end
+  end
 end
