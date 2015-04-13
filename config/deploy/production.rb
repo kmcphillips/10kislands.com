107693cbf2be6e949f99177b366a6de84ba27359
Kevin McPhillips <github@kevinmcphillips.ca>
2015-04-13 17:53:33 -0400

    Add production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
-server '10kislands.com', user: 'todo', roles: %w{web app}, primary: true
+server "198.211.110.159", user: "kevin", roles: %w{web app}, primary: true
 
-set :deploy_to, "/home/todo/10kislands.com"
+set :deploy_to, "/home/kevin/admin.10kislands.com"
+
+set :linked_dirs, fetch(:linked_dirs, []).push("public/system")
